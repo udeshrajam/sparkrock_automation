c26cdd8b9d049036000b4855623dcb10e3c3b507
udesh <udeshraja028@gmail.com>
2024-12-19 16:19:24 +0530

    Test case added: Verify user can sort the product list for "Name Z to A"

---
+++ src/test/java/steps_definitions/product_list_steps.java
@@ -0,0 +1,34 @@
+package steps_definitions;
+
+import io.cucumber.java.bs.A;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en_scouse.An;
+import org.example.pages.productListPage;
+
+public class product_list_steps {
+
+    productListPage productListPage = new productListPage();
+
+
+    @And("select {string} as sorting option")
+    public void selectAsSortingOption(String sortBy) throws InterruptedException {
+        productListPage.dropdownSortOption(sortBy);
+    }
+
+    @And("verify the product list")
+    public void verify_product_list() {
+        productListPage.verifySortedProductList();
+    }
+
+    @And("click add to cart button for {string}")
+    public void add_selected_products_to_cart(String products) throws InterruptedException {
+        productListPage.addItemsToCard(products);
+    }
+
+
+    @And("click your cart button")
+    public void click_your_cart_button() {
+        productListPage.shoppingCartButton();
+    }
+}
